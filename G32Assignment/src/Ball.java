fcf2467c4f1d593b69d2fbd244730f1e34f89e62
JetTheSwagger <darrenlew2004@gmail.com>
2023-12-18 21:12:05 +0800

    changes

--- G32Assignment/src/Ball.java
+++ G32Assignment/src/Ball.java
@@ -1,4 +1,44 @@
 
 public class Ball {
 
+    private String name;
+    private float catchChance;
+    private float useChance;
+
+    public Ball(String name, float catchChance, float useChance) {
+        this.name = name;
+        this.catchChance = catchChance;
+        this.useChance = useChance;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getCatchChance() {
+        return catchChance;
+    }
+
+    public void setCatchChance(float catchChance) {
+        this.catchChance = catchChance;
+    }
+
+    public float getUseChance() {
+        return useChance;
+    }
+
+    public void setUseChance(float useChance) {
+        this.useChance = useChance;
+    }
+
+    @Override
+    public String toString() {
+        return "Ball [name=" + name + ", catchChance=" + catchChance + ", useChance=" + useChance + "]";
+    }
+
+    
 }
---
+++ G32Assignment/src/Pokeball.java
@@ -0,0 +1,8 @@
+public class Pokeball extends Ball{
+
+    public Pokeball(String name, float catchChance, float useChance) {
+        super(name, catchChance, useChance);
+        //TODO Auto-generated constructor stub
+    }
+    
+}
