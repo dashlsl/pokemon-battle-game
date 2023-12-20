f28848c4af13ffa1843b0e0b45efd8ea8e39ff57
dashlsl <dashaynelumsl@gmail.com>
2023-12-20 20:52:34 +0800

    weewoo ball name go brr

--- G32Assignment/src/Balls/Pokeball.java
+++ G32Assignment/src/Balls/Pokeball.java
@@ -3,6 +3,6 @@ public class PokeBall extends Ball {
 
     public PokeBall(String name, float pokeCalChance, float useChance) {
         super(name, pokeCalChance, useChance);
-        //TODO Auto-generated constructor stub
+
     }  
 }
