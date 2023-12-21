94708ebe478723e2a2881042169f9f13dafe1496
sunwayTim <137743545+sunwayTim@users.noreply.github.com>
2023-12-21 13:09:17 +0800

    Update naming of pokemon

--- G32Assignment/src/Game.java
+++ G32Assignment/src/Game.java
@@ -3,14 +3,16 @@ import Balls.*;
 
 public class Game {
     public static void main(String[] args) {
-        WaterPokemon myPokemon = new WaterPokemon("Squirtle", "Caught", "Water", 1, 100, 20, "Design1");
-        Pokemon opponentPokemon = new Pokemon("Charmander", "Wild", "Fire", 1, 100, 15, "Design2");
+        WaterPokemon poke1 = new WaterPokemon("Squirtle", "Caught", "Water", 1, 100, 20, "Design1");
+        Pokemon enem1 = new Pokemon("Charmander", "Wild", "Fire", 1, 100, 15, "Design2");
 
         // Attack opponent with myPokemon
-        myPokemon.attack(opponentPokemon);
+        poke1.attack(enem1);
 
         // Display opponent's health after the attack
-        System.out.println(opponentPokemon.getName() + "'s Health: " + opponentPokemon.getHealth());
-        System.out.println(myPokemon.getName() + "'s Health: " + myPokemon.getHealth());
+        System.out.println(enem1.getName() + "'s Health: " + enem1.getHealth());
+        System.out.println(poke1.getName() + "'s Health: " + poke1.getHealth());
+        
+        
     }
 }
