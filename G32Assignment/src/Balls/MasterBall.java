743b60b053c66b1edf6cea43a990885812c8a68f
JetTheSwagger <darrenlew2004@gmail.com>
2023-12-21 16:27:05 +0800

    Update MasterBall.java

--- G32Assignment/src/Balls/MasterBall.java
+++ G32Assignment/src/Balls/MasterBall.java
@@ -1,9 +1,8 @@
 package Balls;
 
 public class MasterBall extends Ball {
-    private boolean masterCalChance;
 
     public MasterBall(String name, float catchChance, float useChance, boolean calculation, float smallMultiplier, float bigMultiplier){
-        this.masterCalChance = masterCalChance;
+        super(name, catchChance, useChance, calculation, smallMultiplier, bigMultiplier);
     }
 }
